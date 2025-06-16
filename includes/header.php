b526a54e7143926951a3ee30117d1fa7cde929fa
Latihan Git <melonpea12@gmail.com>
2025-06-17 02:51:01 +0700

    update

--- includes/header.php
+++ includes/header.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="header-content">
             <a href="/" class="logo">
-                <div class="logo-icon">JE</div>
+                <img src="/assets/images/logo/Logo-1.png" alt="<?php echo $site_config['site_name']; ?> Logo" class="logo-image">
                 <span class="logo-text"><?php echo $site_config['site_name']; ?></span>
             </a>
-            
+
             <div class="header-actions">
-                <a href="<?php echo $site_config['contact']['saweria']; ?>" 
-                   target="_blank" class="btn btn-primary">
+                <a href="<?php echo $site_config['contact']['saweria']; ?>"
+                    target="_blank" class="btn btn-primary">
                     Saweria
                 </a>
             </div>
